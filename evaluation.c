09d36caead9e317add67dbc0eba4e87b66985c13
Josh Early <jde2738@gmail.com>
2021-08-05 21:04:51 -0400

    Evaluation working. Ex: '- (* 10 10) (+ 1 1 1)', should return the result rather than the AST

--- evaluation.c
+++ evaluation.c
@@ -22,6 +22,45 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
+// use operator sting to see which operation to perform
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) {
+        return x + y;
+    }
+    if (strcmp(op, "-") == 0) {
+        return x - y;
+    }
+    if (strcmp(op, "*") == 0) {
+        return x * y;
+    }
+    if (strcmp(op, "/") == 0) {
+        return x / y;
+    }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    // if tagged as number return in directly
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    // the operator is always second child
+    char* op = t->children[1]->contents;
+
+    // we store the third child in `x`
+    long x = eval(t->children[2]);
+
+    // iterate the remaining children and combining
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
 int main(int argc, char** argv) {
     // create some parsers
     mpc_parser_t* Number    = mpc_new("number");
@@ -55,8 +94,8 @@ int main(int argc, char** argv) {
         // Attempt to parse the user input
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            // on success print and delete the AST
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             // otherwise print and delete the error
