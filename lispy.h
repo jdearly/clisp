08abde2786079eb599c138e817dd8244ae5d8813
Josh Early <jde2738@gmail.com>
2021-08-18 18:01:59 -0400

    a little more cleanup

--- lispy.h
+++ lispy.h
@@ -8,15 +8,17 @@ struct lenv;
 typedef struct lval lval;
 typedef struct lenv lenv;
 
-enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR, LVAL_QEXPR, LVAL_FUN };
-
 typedef lval*(*lbuiltin)(lenv*, lval*);
+
+enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR, LVAL_QEXPR, LVAL_FUN };
 
 struct lenv {
     int count;
     char** syms;
     lval** vals;
 };
+
+
 typedef struct lval {
     int type;
     long num;
@@ -29,31 +31,35 @@ typedef struct lval {
     struct lval** cell; 
 } lval;
 
-lenv* lenv_new(void);
-void lval_del(lval* v);
-void lenv_del(lenv* e);
+lval* lval_join(lval* x, lval* y);
+lval* lval_eval_sexpr(lenv* e, lval* v);
+lval* lval_eval(lenv* e, lval* v);
+lval* lval_read_num(mpc_ast_t* t);
+lval* lval_read(mpc_ast_t* t);
 lval* lval_copy(lval* v);
 lval* lval_err(char* fmt, ...);
-
 lval* lval_fun(lbuiltin func);
 lval* lval_num(long x);
-lval* lval_err(char* fmt, ...); 
 lval* lval_sym(char* s);
 lval* lval_sexpr(void); 
 lval* lval_qexpr(void);
-void lval_del(lval* v);
 lval* lval_add(lval* v, lval* x); 
 lval* lval_pop(lval* v, int i);             
 lval* lval_take(lval* v, int i);
-void lval_print(lval* v);
-void lval_expr_print(lval* v, char open, char close);
-void lval_print(lval* v);
 lval* lval_copy(lval* v);
-void lval_println(lval* v);
-char* ltype_name(int t);
 lval* lval_eval(lenv* e, lval* v);
 lval* lenv_get(lenv* e, lval* k);
+
+lenv* lenv_new(void);
+
+void lval_del(lval* v);
+void lval_print(lval* v);
+void lval_println(lval* v);
+void lval_expr_print(lval* v, char open, char close);
+void lenv_del(lenv* e);
 void lenv_put(lenv* e, lval* k, lval* v);
+
+char* ltype_name(int t);
 
 // Builtins
 lval* builtin(lenv* e, lval* a, char* func);
@@ -72,8 +78,4 @@ void lenv_add_builtin(lenv* e, char* name, lbuiltin func);
 void lenv_add_builtins(lenv* e);
 
 
-lval* lval_join(lval* x, lval* y);
-lval* lval_eval_sexpr(lenv* e, lval* v);
-lval* lval_eval(lenv* e, lval* v);
-lval* lval_read_num(mpc_ast_t* t);
-lval* lval_read(mpc_ast_t* t);
+
